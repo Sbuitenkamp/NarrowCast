6a254958221585a5f8c24efa5a8b8a44293b4dfc
Sbuitenkamp <buitenkamp.developer@gmail.com>
2019-09-19 14:10:20 +0200

    hotfix timer

--- controllers/timer.js
+++ controllers/timer.js
@@ -3,10 +3,10 @@ const dateDiv = document.querySelector("div.footer>div.footer__date>span.footer_
 // initial
 const initialDate = new Date();
 timer.innerHTML = `${initialDate.getHours()}:${initialDate.getMinutes()}:${initialDate.getSeconds()}`;
-dateDiv.innerHTML = `${initialDate.getDay()}/${initialDate.getMonth()}/${initialDate.getFullYear()}`;
+dateDiv.innerHTML = `${initialDate.getDate()}/${initialDate.getMonth() + 1}/${initialDate.getFullYear()}`;
 // timer
 setInterval(() => {
     const date = new Date();
     timer.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    dateDiv.innerHTML = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
+    dateDiv.innerHTML = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 }, 1000);
